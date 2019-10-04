2bac8b229249daa268f4f0b068b78fe99148dcb1
Sakaki <aa31568@gmail.com>
2019-10-04 13:40:14 +0900

    webpack

--- script/ripple.js
+++ script/ripple.js
@@ -1,1 +1,26 @@
-(function () { let ripple, ripples, RippleEffect, loc, cover, coversize, style, x, y, i, num; ripples = document.querySelectorAll(".ripple"); RippleEffect = function (a) { ripple = this; cover = document.createElement("span"); coversize = ripple.offsetWidth; loc = ripple.getBoundingClientRect(); x = a.pageX - loc.left - window.pageXOffset - (coversize / 2); y = a.pageY - loc.top - window.pageYOffset - (coversize / 2); pos = "top:" + y + "px; left:" + x + "px; height:" + coversize + "px; width:" + coversize + "px;"; ripple.appendChild(cover); cover.setAttribute("style", pos); cover.setAttribute("class", "rp-effect"); setTimeout(function () { let list = document.getElementsByClassName("rp-effect"); for (let i = list.length - 1; i >= 0; i--) { list[i].parentNode.removeChild(list[i]) } }, 2000) }; for (i = 0, num = ripples.length; i < num; i++) { ripple = ripples[i]; ripple.addEventListener("mousedown", RippleEffect) } }());
+export function ripple() {
+    let ripple, ripples, RippleEffect, loc, cover, coversize, style, x, y, i, num;
+    ripples = document.querySelectorAll(".ripple");
+    RippleEffect = function (a) {
+        ripple = this;
+        cover = document.createElement("span");
+        coversize = ripple.offsetWidth;
+        loc = ripple.getBoundingClientRect();
+        x = a.pageX - loc.left - window.pageXOffset - (coversize / 2);
+        y = a.pageY - loc.top - window.pageYOffset - (coversize / 2);
+        var pos = "top:" + y + "px; left:" + x + "px; height:" + coversize + "px; width:" + coversize + "px;";
+        ripple.appendChild(cover);
+        cover.setAttribute("style", pos);
+        cover.setAttribute("class", "rp-effect");
+        setTimeout(function () {
+            let list = document.getElementsByClassName("rp-effect");
+            for (let i = list.length - 1; i >= 0; i--) {
+                list[i].parentNode.removeChild(list[i])
+            }
+        }, 2000)
+    };
+    for (i = 0, num = ripples.length; i < num; i++) {
+        ripple = ripples[i];
+        ripple.addEventListener("mousedown", RippleEffect)
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,19 @@
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./script/index.js",
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/script'
+    },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            path: path.resolve(__dirname, 'dist'),
-                            filename: 'bundle.js',
-                            presets: [
-                                "@babel/preset-env"
-                            ]
-                        }
-                    }
-                ]
-            }
-        ]
+        rules: [{
+            test: /\.js$/,
+            // use: [{
+            //     loader: "babel-loader",
+            //     options: {
+            //         presets: ["@babel/preset-env"]
+            //     }
+            // }]
+        }]
     }
 };
