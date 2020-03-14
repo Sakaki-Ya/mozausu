16c4f6f8f67da3f259d98f39208e5a6b2b8c5414
Sakaki <aa3168@gmail.com>
2020-03-14 12:52:16 +0900

    scss + gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const gulp = require("gulp");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+
+const sassOptions = {
+  options: {
+    outputStyle: "compressed",
+    sourceMap: false,
+    sourceComment: false
+  }
+};
+
+gulp.task("style", () => {
+  const src = "./style/style.scss";
+  const dest = "./";
+  return gulp
+    .src(src)
+    .pipe(
+      plumber({
+        errorHandler: notify.onError("Error: <%= error.message %>")
+      })
+    )
+    .pipe(sass(sassOptions))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(dest))
+    .pipe(
+      notify({
+        title: "scssをコンパイルしました！",
+        message: new Date()
+      })
+    );
+});
